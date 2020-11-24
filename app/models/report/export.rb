756c7ce8f410f73824f8761457efc9b3ba7e863a
William Murphy <wmurphy@repay.com>
2020-11-23 19:50:28 -0600

    no message

--- app/models/report/export.rb
+++ app/models/report/export.rb
@@ -9,13 +9,12 @@ class Report::Export
     @csv = CSV.generate(headers: true) do |csv|
       csv << ["Name","Phone","Email","Address","Entered At"]
       all_prizes.each do |k,v|
-        puts ""
         csv << [k]
         v.each do |item|
           csv << [
             item.name,
             item.phone,
-            item.email,
+            item.email_address,
             item.address,
             item.entered_at
           ]
@@ -27,6 +26,6 @@ class Report::Export
   private
 
   def all_prizes
-    UserPrize.select('users.name, users.phone, users.email, users.address, user_prizes.created_at as entered_at, prizes.name as prize_name').joins(:prize, :user).all.to_a.group_by {|r| r['prize_name']}
+    UserPrize.select('users.name, users.phone, users.email as email_address, users.address, user_prizes.created_at as entered_at, prizes.name as prize_name').joins(:prize, :user).all.to_a.group_by {|r| r['prize_name']}
   end
 end
